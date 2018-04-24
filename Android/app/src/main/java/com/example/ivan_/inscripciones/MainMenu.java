2b63900868d48ec0006f91111bb5b76b3d0d7a96
Unknown <ivan_bustillos@hotmail.com>
2018-04-23 19:17:07 -0600

    Trabajo que no se nota del menu principal para el Jamberguer meniu

--- Android/app/src/main/java/com/example/ivan_/inscripciones/MainMenu.java
+++ Android/app/src/main/java/com/example/ivan_/inscripciones/MainMenu.java
@@ -1,13 +1,24 @@
 package com.example.ivan_.inscripciones;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainMenu extends AppCompatActivity {
 
+  private DrawerLayout mDrawerLayout;
+  private ActionBarDrawerToggle mToggle;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_menu);
+    mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+    mToggle = new ActionBarDrawerToggle(MainMenu.this, mDrawerLayout,R.string.open, R.string.close);
+    mDrawerLayout.addDrawerListener(mToggle);
+    mToggle.syncState();
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
+
   }
 }
